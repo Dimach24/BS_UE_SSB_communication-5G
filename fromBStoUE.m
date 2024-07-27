38c928b29d2228df0dab47bf40ad72038e80664a
D.mon 24 <dchaminov@ya.ru>
2024-07-27 12:52:54 +0300

    case=B, Lmax_ as HALF hotfix, RG generation added

--- fromBStoUE.m
+++ fromBStoUE.m
@@ -6,18 +6,25 @@ addpath(genpath(pwd))
 
 NCellId=17;
 
-caseL   = 'D';
-scs     = 120;
-Lmax_   = 64;   % amount of SSB in the FRAME (N.B. not half-frame)
-mu      = 3;
-k_SSB   = 15;
+caseL   = 'B';
+scs     = 30;
+pointA  = 4.4;  % GHz
+Lmax_   = 8;   % amount of SSB in the HALF-FRAME
+mu      = 1;
+k_SSB   = 20;
 SFN_MSB = 37;
 SFN_LSB = 1;
+tran_bandwidth = 60;
+toff    =0;
+foff    =k_SSB;
 
+
+
+kSSB_bin=int2bit(k_SSB,4).';
 MIB     =[...
     int2bit(SFN_MSB,6).',   ... % SFN_MSB
     (scs==15||scs==60),     ... % scs15or60
-    int2bit(k_SSB,4).'      ... % kSsbLsb=4
+    kSSB_bin(1:4)           ... % kSsbLsb
     1,                      ... % dmrs pos3
     1,1,1,1,                ... % sib.RB=15
     0,1,0,1,    ... % sib.searchSpaceZero=5
@@ -28,17 +35,29 @@ MIB     =[...
     ];
 %%
 
-bits=zeros(Lmax_,864);
+bits=zeros(864,Lmax_*2);
 
-for issb=0:(Lmax_-1)
-    issb_bin=int2bit(mod(issb,Lmax_/2),6,true);
-    bits(issb+1,:)=PbchGenerator.generatePbch(...
+for issb=0:(Lmax_*2-1)
+    issb_bin=int2bit(mod(issb,Lmax_),6,true);
+    bits(:,issb+1)=PbchGenerator.generatePbch(...
         MIB,...
         SFN_MSB+SFN_LSB,...
-        issb>=Lmax_/2,...
-        issb_bin(1:3),...
+        issb>=Lmax_,...
+        [k_SSB>=16,0,0],...
         NCellId, ...
         Lmax_...
         );
 end
 
+%%
+
+rg=ResourceTransmitter.GenerateFrame(bits,NCellId,caseL,pointA,tran_bandwidth,toff,foff,[1,1,0.85,0.9]);
+
+%%
+plt=pcolor(abs(rg(1:301,1:50)));
+plt.EdgeColor='none';
+ca=gca();
+ca.YDir='normal';
+xlim([1,50]);
+xlabel('l+1 (номер OFDM символа +1)')
+ylabel('k (номер поднесущей)')
